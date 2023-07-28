00afc1e5a726e3423796319a1132ec34af3fb316
WetDesertRock <wetdesertrock@gmail.com>
2023-07-27 22:52:07 -0700

    Fix __dirname generation

--- src/promptgenerator.js
+++ src/promptgenerator.js
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import { shuffleArray, randomChoice } from './randomUtil.js'
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = path.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let focusLists = [
